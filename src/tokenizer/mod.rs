d22e507746730f7b7f3683e5a42292abdf47e009
Ygg01 <y.laughing.man.y@gmail.com>
2021-11-18 21:06:12 +0100

    First commit

---
+++ src/tokenizer/mod.rs
@@ -0,0 +1,89 @@
+use std::io::BufRead;
+use encoding_rs::Encoding;
+
+pub struct Tokenizer<R: BufRead> {
+    pub(crate) reader: R,
+    state: TokenState,
+    #[cfg(feature = "encoding")]
+    encoding: &'static Encoding,
+    #[cfg(feature = "encoding")]
+    is_encoding_set: bool,
+}
+
+impl<R: BufRead> Tokenizer<R>{
+    pub fn from_reader(reader: R) -> Tokenizer<R> {
+        Tokenizer {
+            reader,
+            state: TokenState::Data,
+            #[cfg(feature = "encoding")]
+            encoding: ::encoding_rs::UTF_8,
+            #[cfg(feature = "encoding")]
+            is_encoding_set: false,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+#[doc(hidden)]
+pub enum TokenState {
+    Data,
+    TagState,
+    EndTagState,
+    EndTagName,
+    EndTagNameAfter,
+    Pi,
+    PiTarget,
+    PiTargetAfter,
+    PiData,
+    PiAfter,
+    MarkupDecl,
+    CommentStart,
+    CommentStartDash,
+    Comment,
+    CommentLessThan,
+    CommentLessThanBang,
+    CommentLessThanBangDash,
+    CommentLessThanBangDashDash,
+    CommentEnd,
+    CommentEndDash,
+    CommentEndBang,
+    Cdata,
+    CdataBracket,
+    CdataEnd,
+    TagName,
+    TagEmpty,
+    TagAttrNameBefore,
+    TagAttrName,
+    TagAttrNameAfter,
+    TagAttrValueBefore,
+    TagAttrValue(AttrValueKind),
+    Doctype,
+    BeforeDoctypeName,
+    DoctypeName,
+    AfterDoctypeName,
+    AfterDoctypeKeyword(DoctypeKind),
+    BeforeDoctypeIdentifier(DoctypeKind),
+    DoctypeIdentifierDoubleQuoted(DoctypeKind),
+    DoctypeIdentifierSingleQuoted(DoctypeKind),
+    AfterDoctypeIdentifier(DoctypeKind),
+    BetweenDoctypePublicAndSystemIdentifiers,
+    BogusDoctype,
+    BogusComment,
+    Quiescent,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[doc(hidden)]
+pub enum AttrValueKind {
+    Unquoted,
+    SingleQuoted,
+    DoubleQuoted,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[doc(hidden)]
+pub enum DoctypeKind {
+    Public,
+    System,
+}
+
